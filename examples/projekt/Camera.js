7ae882091f451faaee1a87e6f84cc72a4b3a28b0
tadejlenart <76212638+tadejlenart@users.noreply.github.com>
2021-12-30 09:08:32 +0100

    Add files via upload

---
+++ examples/projekt/Camera.js
@@ -0,0 +1,134 @@
+import { vec3, mat4 } from '../../lib/gl-matrix-module.js';
+
+import { Utils } from './Utils.js';
+import { Node } from './Node.js';
+import { Arrow } from './Arrow.js';
+
+export class Camera extends Node {
+
+    constructor(options) {
+        super(options);
+        Utils.init(this, this.constructor.defaults, options);
+        this.mousemoveHandler = this.mousemoveHandler.bind(this);
+        this.keydownHandler = this.keydownHandler.bind(this);
+        this.keyupHandler = this.keyupHandler.bind(this);
+
+        //bind event handler to camera object
+        this.mouseDownHandler = this.mouseDownHandler.bind(this);
+
+        this.keys = {};
+    }
+
+    update(dt) {
+        const c = this;
+
+        const forward = vec3.set(vec3.create(),
+            -Math.sin(c.rotation[1]), 0, -Math.cos(c.rotation[1]));
+        const right = vec3.set(vec3.create(),
+            Math.cos(c.rotation[1]), 0, -Math.sin(c.rotation[1]));
+
+
+        // 1: add movement acceleration
+        let acc = vec3.create();
+        if (this.keys['KeyW']) {
+            vec3.add(acc, acc, forward);
+        }
+        if (this.keys['KeyS']) {
+            vec3.sub(acc, acc, forward);
+        }
+        if (this.keys['KeyD']) {
+            vec3.add(acc, acc, right);
+        }
+        if (this.keys['KeyA']) {
+            vec3.sub(acc, acc, right);
+        }
+
+        // 2: update velocity
+        vec3.scaleAndAdd(c.velocity, c.velocity, acc, dt * c.acceleration);
+
+        // 3: if no movement, apply friction
+        if (!this.keys['KeyW'] &&
+            !this.keys['KeyS'] &&
+            !this.keys['KeyD'] &&
+            !this.keys['KeyA'])
+        {
+            vec3.scale(c.velocity, c.velocity, 1 - c.friction);
+        }
+
+        // 4: limit speed
+        const len = vec3.len(c.velocity);
+        if (len > c.maxSpeed) {
+            vec3.scale(c.velocity, c.velocity, c.maxSpeed / len);
+        }
+    }
+
+    enable() {
+        document.addEventListener('mousemove', this.mousemoveHandler);
+        document.addEventListener('keydown', this.keydownHandler);
+        document.addEventListener('keyup', this.keyupHandler);
+
+        //add mouse clik event handler
+        document.addEventListener('mousedown', this.mouseDownHandler);
+    }
+
+    disable() {
+        document.removeEventListener('mousemove', this.mousemoveHandler);
+        document.removeEventListener('keydown', this.keydownHandler);
+        document.removeEventListener('keyup', this.keyupHandler);
+
+        for (let key in this.keys) {
+            this.keys[key] = false;
+        }
+
+        //remove mouse clik event handler
+        document.removeEventListener('mousedown', this.mouseDownHandler);
+    }
+
+    mousemoveHandler(e) {
+        const dx = e.movementX;
+        const dy = e.movementY;
+        const c = this;
+
+        c.rotation[0] -= dy * c.mouseSensitivity;
+        c.rotation[1] -= dx * c.mouseSensitivity;
+
+        const pi = Math.PI;
+        const twopi = pi * 2;
+        const halfpi = pi / 2;
+
+        if (c.rotation[0] > halfpi) {
+            c.rotation[0] = halfpi;
+        }
+        if (c.rotation[0] < -halfpi) {
+            c.rotation[0] = -halfpi;
+        }
+
+        c.rotation[1] = ((c.rotation[1] % twopi) + twopi) % twopi;
+
+    }
+
+    keydownHandler(e) {
+        this.keys[e.code] = true;
+    }
+
+    keyupHandler(e) {
+        this.keys[e.code] = false;
+    }
+
+    mouseDownHandler(e){
+        console.log(this);
+        //const arrow = new Arrow(this.children[0]);
+        //console.log(arrow);
+        // const arrwoNode = this.clone(arrow);
+        // console.log(arrwoNode);
+    }
+
+}
+
+Camera.defaults = {
+    velocity         : [0, 0, 0],
+    mouseSensitivity : 0.002,
+    maxSpeed         : 3,
+    friction         : 0.2,
+    acceleration     : 20
+};
